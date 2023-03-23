901392b9fd9975330e6b9665647ab4b31f41efc0
hyun <hyunfree0106@gmail.com>
2023-03-23 11:19:10 +0900

    f2 작성중

--- basic/f2.py
+++ basic/f2.py
@@ -1,1 +1,20 @@
-# 새로운 브런치 basic에서 작업을 진행됨
+'''
+    라우트 추가 -> URL 추가
+
+'''
+import os
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+from flask_cors import CORS
+
+# Flask 객체 인스턴스 생성
+app = Flask(__name__)
+
+# 접속 URL 설정
+@app.route('/')
+def hello():
+    return "hello world!"
+
+if __name__ == '__main__':
+    # Web 상에 기본 포트 : http -> 80 -> 생략가능
+    # 나중에 웹서버(apache, nginx)와 연동
+    app.run(host = '0.0.0.0', port = 5000, debug=True)
