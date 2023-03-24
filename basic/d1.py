e77570bb0a952f8c0ee312f1e1b661386530044f
hyun <hyunfree0106@gmail.com>
2023-03-24 13:52:44 +0900

    feat : DB 연결 및 접속 종료 기본형

--- basic/d1.py
+++ basic/d1.py
@@ -41,13 +41,71 @@
     ENGINE=InnoDB
     ;
     
-    6. 테이블 변경
+    # 컬럼 추가 : ADD COLUMN
+    # 컬럼 변경 : MODIFY COLUMN
+    # 컬럼 이름 포함 변경 : CHANGE COLUMN
+    # 컬럼 삭제 : DROP  COLUMN
+    # 테이블 이름 변경 : RENAME
+    # ALTER TABLE <테이블명> CHANGE COLUMN <OLD 컬럼> <NEW 컬럼> <데이터 타입> [FIRST|AFTER <컬럼명>]
+
     ALTER TABLE `users`
 	CHANGE COLUMN `uid` `uid` VARCHAR(32) NOT NULL COMMENT '고객 로그인 ID' COLLATE 'utf8mb4_general_ci' AFTER `id`,
 	CHANGE COLUMN `upw` `upw` VARCHAR(128) NOT NULL COMMENT '고객 로그인 PW' COLLATE 'utf8mb4_general_ci' AFTER `uid`,
 	CHANGE COLUMN `name` `name` VARCHAR(32) NOT NULL COMMENT '고객 이름' COLLATE 'utf8mb4_general_ci' AFTER `upw`,
 	CHANGE COLUMN `regdate` `regdate` TIMESTAMP NOT NULL DEFAULT current_timestamp() COMMENT '고객 가입일' AFTER `name`;
 
+    6. 회원가입 -> insert
+    - INSERT INTO [<DB명>.]<테이블명> (컬럼명, 컬럼명, ...) values (값, 값, ...);
+    - `` : 키워드를 테이블명, 컬럼명등등 사용하면 벡틱를 통해 처리 가능
+    INSERT INTO 
+        `ml_db`.`users` 
+            (`uid`, `upw`, `name`, `regdate`) 
+        VALUES 
+            ('guest', '1234', '게스트', now());    
+    
+    7. 회원 정보 수정 -> update
+    
+    8. 회원 탈퇴 -> delete
+        - 1년간 보관, 완전 삭제?
+    
+    9. 로그인 -> select (쿼리의 분량이 가장 많다)
+    -- 회원 가입 여부 조회 -> 로그인 처리 쿼리 
+    -- 대소문자 구분이 안함 
+    -- 제시한 ID, PW와 일치하는 row 데이터를 가져와서 uid, name, regdate 값을 출력하시오.
+    SELECT 
+        uid, `name`
+    FROM 
+        users
+    WHERE 
+        uid = 'guest'
+    AND
+        upw = '1234';
 """
 
-import pymysql as my
+import pymysql
+
+connection = None
+
+try:
+    # 1. 접속
+    connection = pymysql.connect(
+        host="localhost",  # 127.0.0.1 (서버측)
+        # port = 3306, # 포트
+        user="root",  # 사용자 계정, root 계정 외 사용 권장
+        password="1q2w3e4r",  # 비밀번호
+        database="ml_db",  # 접속할 DB
+        # cursorclass = pymysql.cursors.DictCursor
+    )
+    print("접속 성공")
+
+except Exception as e:
+    print("접속 오류", e)
+
+else:
+    print("접속시 문제 없었음")
+
+finally:
+    # 2. 접속 종료 (I/O)-> close()
+    if connection:
+        connection.close()
+    print("접속 종료 성공")
