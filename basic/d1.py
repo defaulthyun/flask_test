5874517d92927bd0b79a0c9f865a230eed2b2f64
hyun <hyunfree0106@gmail.com>
2023-03-24 11:31:08 +0900

    feat : 고객 테이블 생성 및 업데이트(스키마)를 업데이트(수정)

--- basic/d1.py
+++ basic/d1.py
@@ -22,10 +22,32 @@
     4. 현재 작업 할 DB 지정
     use ml_db;
     
+    5. 고객 테이블 생성
     
+    # USING BTREE : 검색 속도를 높이는 방식 중 하나로 특정 컬럼을 지정
     
+    CREATE TABLE `users` (
+        `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT '고객 고유 관리 ID',
+        `uid` VARCHAR(32) NOT NULL COMMENT '고객 로그인 ID' COLLATE 'utf8mb4_general_ci',
+        `upw` VARCHAR(128) NOT NULL COMMENT '고객 로그인 PW' COLLATE 'utf8mb4_general_ci',
+        `name` VARCHAR(32) NOT NULL COMMENT '고객 이름' COLLATE 'utf8mb4_general_ci',
+        `regdate` TIMESTAMP NOT NULL DEFAULT current_timestamp() COMMENT '고객 가입일',
+        PRIMARY KEY (`id`) USING BTREE,
+        UNIQUE INDEX `id` (`id`) USING BTREE,
+        UNIQUE INDEX `uid` (`uid`) USING BTREE
+    )
+    COMMENT='고객 테이블'
+    COLLATE='utf8mb4_general_ci'
+    ENGINE=InnoDB
+    ;
     
-    
+    6. 테이블 변경
+    ALTER TABLE `users`
+	CHANGE COLUMN `uid` `uid` VARCHAR(32) NOT NULL COMMENT '고객 로그인 ID' COLLATE 'utf8mb4_general_ci' AFTER `id`,
+	CHANGE COLUMN `upw` `upw` VARCHAR(128) NOT NULL COMMENT '고객 로그인 PW' COLLATE 'utf8mb4_general_ci' AFTER `uid`,
+	CHANGE COLUMN `name` `name` VARCHAR(32) NOT NULL COMMENT '고객 이름' COLLATE 'utf8mb4_general_ci' AFTER `upw`,
+	CHANGE COLUMN `regdate` `regdate` TIMESTAMP NOT NULL DEFAULT current_timestamp() COMMENT '고객 가입일' AFTER `name`;
+
 """
 
 import pymysql as my
