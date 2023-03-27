201b3e7685c74d667f4decf3fff097a6f34c2aed
hyun <hyunfree0106@gmail.com>
2023-03-27 13:45:13 +0900

    feat : 블루 프린트 적용

--- advance/step2_app_factory/service/__init__.py
+++ advance/step2_app_factory/service/__init__.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask
-from flask_cors import CORS
 
 """
     create_app는 플라스크 내부에서 정의된 함수명(수정 X)
@@ -8,15 +7,29 @@ from flask_cors import CORS
     차후, 다른 모듈에서는 flask.current_app 이라는 변수로 app을 접근 할 수 있다(모듈가져오기)
 """
 
-## Flask 객체 인스턴스 생성
-
 
 def create_app():
     app = Flask(__name__)
-
-    # 접속 URL 설정
-    @app.route("/")
-    def home():
-        return "home page custom_app"
-
+    init_blueprint(app)
     return app
+
+
+def init_blueprint(app):
+    # app에 블루프린트 객체를 등록한다
+
+    # 실습 http://127.0.0.1:5000/auth : 접속 시 인증 홈페이지 내용이 나오도록
+    # auth 관련 블루프린트를 구현하시오
+
+    # 불루프린트로 정의된 개별 페이지 관련 내용 로드
+    from .controllers import main_controller
+    from .controllers import auth_controller
+
+    # 이 위치에서는 service를 생략하고 표현 가능
+    # from service.controllers import bp_main
+    from .controllers import bp_main, bp_auth
+
+    # 플라스크 객체에 블루 프린트 등록
+    app.register_blueprint(bp_main)
+    app.register_blueprint(bp_auth)
+
+    pass
